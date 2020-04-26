588d8abe4dac3326ecff4302f0121c131aeed16e
Tiago Versuti <tiagoversuti@gmail.com>
2020-04-26 02:11:56 -0300

    Setup - Controllers e Startup

---
+++ MinhaAPICompleta/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validacao de notificacoes de erro
+
+        // validacao de modelstate
+
+        // validacao de operacao de negocios
+    }
+}
