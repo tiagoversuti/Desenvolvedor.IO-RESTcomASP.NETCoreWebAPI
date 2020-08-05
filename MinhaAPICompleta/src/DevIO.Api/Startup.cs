6aacf5a0012a698201aad2aa862f60dc692cef2b
Tiago Versuti <tiagoversuti@gmail.com>
2020-08-04 22:11:38 -0300

    Implementando o ASP.NET Identity

--- MinhaAPICompleta/src/DevIO.Api/Startup.cs
+++ MinhaAPICompleta/src/DevIO.Api/Startup.cs
@@ -26,6 +26,8 @@ namespace DevIO.Api
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddIdentityConfiguration(Configuration);
+
             services.AddAutoMapper(typeof(Startup));
 
             services.WebApiConfig();
@@ -45,6 +47,8 @@ namespace DevIO.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseAuthentication();
 
             app.UseMvcConfiguration();
         }
